156ac73474e9b41ddcf9d3b6a9db405e8ae11d53
Xavi Ojeda <xojeda@engel.es>
2021-08-02 03:35:39 +0200

    inventory making and tweaks

--- src/commands/misc/inventory.ts
+++ src/commands/misc/inventory.ts
@@ -1,20 +1,38 @@
-import { Message, MessageEmbed } from "discord.js";
+import { Message, MessageEmbed  } from "discord.js";
 import { Bot } from "../../client/Client";
 import { GearInventory } from "../../database/GearInventory/Model/GearInventory";
 import { Item } from "../../database/Item/Model/Item";
-import { RunFunction } from '../../interfaces/Command'
+import { RunFunction } from '../../interfaces/Command';
+import { Avatar } from "../../database/Avatar/Model/Avatar";
+import { MainStats } from "../../database/MainStats/Model/MainStats";
 
 export const run: RunFunction = async (client: Bot, message: Message) => {
+  const avatar: Avatar = await Avatar.findByPk(message.author.id, { raw: false, include: [{model: MainStats, as: 'mainStats'}]})
+  const avatarValues = <Avatar>avatar.get({plain: true});
+
+  const msg = new MessageEmbed()
+  .setColor('#0099ff')
+  .setTitle('Revisa tus objetos')
+  .setURL('https://discord.com')
+  .setDescription(`Hola \`${avatarValues.username}\`,\nthis is your inventory:`)
 
   const inventory: GearInventory[] = await GearInventory.findAll({ where: { avatarId: message.author.id}, include: [{model: Item, as: 'item' }], raw: false})
-  inventory.forEach((value) => {
-    const a = <GearInventory>value.get({plain: true})
-    console.log('GEAR INVENTORY:')
-    console.log(a)
-    console.log('ITEM INFO:');
-    console.log(a.item)
+  inventory.forEach((v) => {
+    const inv = <GearInventory>v.get({plain: true})
+    const emoFind = inv.item.itemSubtypeId
+    let emo : string = ""
+
+    switch (emoFind){
+      case 1: emo = "🗡"; break
+      case 2: emo = "🔨"; break
+      case 4: emo = "🜜";break
+    };
+
+    msg.addFields(
+      { name: '\u200B', value:`${emo} ${inv.item.name} ➣ ${inv.item.description}`}
+    );
   })
-  
+  return message.channel.send(msg)
 };
 
 export const name: string = 'inventory';
