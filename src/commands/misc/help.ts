38d3e7b69113b76ab6fd1a6194bcf36799167f02
GuillemPM <guillem.martin.p@gmail.com>
2021-08-08 14:10:39 +0200

    Optimize command help

--- src/commands/misc/help.ts
+++ src/commands/misc/help.ts
@@ -3,19 +3,17 @@ import { Bot } from "../../client/Client";
 import { RunFunction } from "../../interfaces/Command";
 
 export const run: RunFunction = async (client: Bot, message: Message) => {
-  let i: any = 0
-  const msg = new MessageEmbed
-  msg
+  const msg = new MessageEmbed()
     .setColor('#0099ff')
 	  .setTitle('Check te commands!')
 	  .setURL('https://discord.com')
 
-  for(i in client.commands.array()){
-    const commnd = client.commands.array()[i]
-    if(commnd.permisions != 1){
-      msg.addField(`${commnd.name}`, `${commnd.description} \n Aliases: ${commnd.aliases? commnd.aliases.join(', ') :'No Aliases available'}`)
+  client.commands.forEach(cmd => {
+    if (cmd.permisions !== 1) {
+      msg.addField(`[${cmd.name}]`, `${cmd.description}\nAliases: ${cmd.aliases ? cmd.aliases.join(', ') : 'No aliases available'}`)
     }
-  }
+  })
+  
   return message.channel.send(msg)
 }
 
