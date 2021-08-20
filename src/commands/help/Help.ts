64b96f1c2688226cb39a129b067f4df47c9fc40b
GuillemPM <guillem.martin.p@gmail.com>
2021-08-20 16:03:41 +0200

    .

--- src/commands/help/Help.ts
+++ src/commands/help/Help.ts
@@ -10,49 +10,44 @@ export class Help implements Command {
   public subcommands: Collection<string, Command> = new Collection();
 
   public run: RunFunction = async (client: Bot, message: Message, args: string[]) => {
-    if(!args.length){
+    if (!args.length) {
 
       const msg = new MessageEmbed()
         .setColor('#cd7f32')
         .setTitle('Check te commands!')
         .setURL('https://discord.com')
-    
+
       client.commands.forEach(cmd => {
         if (cmd.permisions !== 1) {
           msg.addField(`[${cmd.name}]`, `${cmd.description}\nAliases: ${cmd.aliases ? cmd.aliases.join(', ') : 'No aliases available'}`)
         }
       })
       return message.channel.send(msg)
-    } else {
-      if(client.commands.has(args[0])){
-        const cmd : Command = client.commands.get(args[0])
-        
-        let subcommandsDescription: string = '';
-        cmd.subcommands.forEach(scmd => {
-          subcommandsDescription = subcommandsDescription.concat(`**${scmd.name}**: ${scmd.description}\nAliases: \`${scmd.aliases.join(', ')}\``)
-        })
+    } 
+    
+    const commandName: string = args[0];
+    if (commandName) {
+      const cmd: Command = client.commands.get(commandName) || client.commands.find(command => command.aliases && command.aliases.includes(commandName));
+      
+      if (!cmd) return message.channel.send(`The command \`${commandName}\` doesn't exists`)
+      let subcommandsDescription: string = '';
+      cmd.subcommands.forEach(scmd => {
+        subcommandsDescription = subcommandsDescription.concat(`**${scmd.name}**: ${scmd.description}\nAliases: \`${scmd.aliases.join(', ')}\``)
+      })
 
-        let cmdTitle: string = `━━ ${cmd.name[0].toUpperCase() + cmd.name.slice(1)} `;
-        
-        for( let i = 0; i < 14 - Math.round(cmd.name.length / 5); i++) {
-          cmdTitle = cmdTitle.concat('━')
-        }
+      let cmdTitle: string = `━━ ${cmd.name[0].toUpperCase() + cmd.name.slice(1)} `;
 
-
-        const msg = new MessageEmbed()
-          .setColor('#0099ff')
-          .setTitle(cmdTitle)
-          .setURL('https://discord.com')
-          .setDescription(`${cmd.description}${ cmd.subcommands ? `\n\n▫**Subcommands**▫\n${subcommandsDescription}`: ''}`)
-        
-        return message.channel.send(msg)
-      }else{
-        const msg = new MessageEmbed()
-          .setColor('#0099ff')
-          .setTitle('Watch out that command does not exist')
-          .setURL('https://discord.com')
-        return message.channel.send(msg)
+      for (let i = 0; i < 14 - Math.round(cmd.name.length / 5); i++) {
+        cmdTitle = cmdTitle.concat('━')
       }
+
+      const msg = new MessageEmbed()
+        .setColor('#0099ff')
+        .setTitle(cmdTitle)
+        .setURL('https://discord.com')
+        .setDescription(`${cmd.description}${cmd.subcommands ? `\n\n▫**Subcommands**▫\n${subcommandsDescription}` : ''}`)
+
+      return message.channel.send(msg)
     }
-  }  
+  }
 }
